8d0c9c3e565ebeb4d9ae3e9aa1503acf066569cb
Aliaksei Budavei <oytis@yandex.ru>
2020-10-14 19:06:58 +0300

    Create a service module declaration

---
+++ readywriter/readywriter-service/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * Defines a sink-writing service.
+ */
+module org.zzzyxwvut.readywriter.service
+{
+	requires static org.zzzyxwvut.julics.annotations;
+
+	requires transitive org.zzzyxwvut.julics.core;
+	requires transitive org.zzzyxwvut.julics.naming;
+}
