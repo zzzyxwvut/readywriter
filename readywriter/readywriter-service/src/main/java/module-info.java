c240270b397d50d8e8bf58cf016453a2d6450b3b
Aliaksei Budavei <oytis@yandex.ru>
2020-10-21 21:20:19 +0300

    Export the readywriter and readywriter.service packages

--- readywriter/readywriter-service/src/main/java/module-info.java
+++ readywriter/readywriter-service/src/main/java/module-info.java
@@ -10,5 +10,8 @@ module org.zzzyxwvut.readywriter.service
 	requires transitive org.zzzyxwvut.julics.core;
 	requires transitive org.zzzyxwvut.julics.naming;
 
+	exports org.zzzyxwvut.readywriter.service;
+	exports org.zzzyxwvut.readywriter;
+
 	uses org.zzzyxwvut.readywriter.ReadyWriter;
 }
