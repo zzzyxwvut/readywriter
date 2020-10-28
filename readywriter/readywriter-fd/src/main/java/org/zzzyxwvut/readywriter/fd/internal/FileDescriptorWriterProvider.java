2a04783c33255e777bedc8fd1bc9e62b7e72d770
Aliaksei Budavei <oytis@yandex.ru>
2020-10-28 19:18:09 +0300

    Make a FileDescriptorWriterProvider extension of ReadyWriter

---
+++ readywriter/readywriter-fd/src/main/java/org/zzzyxwvut/readywriter/fd/internal/FileDescriptorWriterProvider.java
@@ -0,0 +1,10 @@
+package org.zzzyxwvut.readywriter.fd.internal;
+
+import org.zzzyxwvut.readywriter.ReadyWriter;
+import org.zzzyxwvut.readywriter.annotation.Namable;
+
+/** A file-descriptor-writing {@code ReadyWriter}. */
+@Namable("org.zzzyxwvut.readywriter.FileDescriptorWriterProvider")
+public interface FileDescriptorWriterProvider extends ReadyWriter
+{
+}
