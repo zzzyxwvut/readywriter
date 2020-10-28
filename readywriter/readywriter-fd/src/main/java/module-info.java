014282b8a43627ec84b12faf1c008175d78990d1
Aliaksei Budavei <oytis@yandex.ru>
2020-10-28 18:56:07 +0300

    Create an fd module declaration

---
+++ readywriter/readywriter-fd/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * Provides a file-descriptor-writing implementation of {@code ReadyWriter}.
+ */
+module org.zzzyxwvut.readywriter.fd
+{
+	requires static org.zzzyxwvut.julics.annotations;
+
+	requires org.zzzyxwvut.julics.core;
+	requires org.zzzyxwvut.julics.naming;
+	requires org.zzzyxwvut.readywriter.service;
+}
