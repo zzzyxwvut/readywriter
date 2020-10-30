05236deb27e61506fff7c8e24b6138450b421106
Aliaksei Budavei <oytis@yandex.ru>
2020-10-31 01:08:10 +0300

    Add a provider method

--- readywriter/readywriter-fd/src/main/java/org/zzzyxwvut/readywriter/fd/internal/FileDescriptorWriterProvider.java
+++ readywriter/readywriter-fd/src/main/java/org/zzzyxwvut/readywriter/fd/internal/FileDescriptorWriterProvider.java
@@ -7,4 +7,18 @@ import org.zzzyxwvut.readywriter.annotation.Namable;
 @Namable("org.zzzyxwvut.readywriter.FileDescriptorWriterProvider")
 public interface FileDescriptorWriterProvider extends ReadyWriter
 {
+	/**
+	 * Creates an instance of this service provider. It is set to write to
+	 * the standard output channel, using the UTF-8 character set.
+	 *
+	 * @return an instance of a service provider
+	 * @see java.lang.System#out
+	 * @see java.util.ServiceLoader.Provider#type()
+	 * @see org.zzzyxwvut.readywriter.service.Lookup#names()
+	 * @see org.zzzyxwvut.readywriter.service.FileDescriptorWriterVisitor
+	 */
+	static FileDescriptorWriterProvider provider()
+	{
+		return new FileDescriptorWriter();
+	}
 }
