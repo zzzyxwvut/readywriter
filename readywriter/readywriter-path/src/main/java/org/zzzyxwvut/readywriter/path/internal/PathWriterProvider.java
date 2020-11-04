0d34057178b15bd24aa7f87a511e07b0028241e7
Aliaksei Budavei <oytis@yandex.ru>
2020-11-04 19:52:35 +0300

    Make a PathWriterProvider extension of ReadyWriter

---
+++ readywriter/readywriter-path/src/main/java/org/zzzyxwvut/readywriter/path/internal/PathWriterProvider.java
@@ -0,0 +1,10 @@
+package org.zzzyxwvut.readywriter.path.internal;
+
+import org.zzzyxwvut.readywriter.ReadyWriter;
+import org.zzzyxwvut.readywriter.annotation.Namable;
+
+/** A path-writing {@code ReadyWriter}. */
+@Namable("org.zzzyxwvut.readywriter.PathWriterProvider")
+public interface PathWriterProvider extends ReadyWriter
+{
+}
