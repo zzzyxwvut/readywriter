b363b09f68575830a878a4b50666615d45165daf
Aliaksei Budavei <oytis@yandex.ru>
2020-11-06 23:54:25 +0300

    Add a provider method

--- readywriter/readywriter-path/src/main/java/org/zzzyxwvut/readywriter/path/internal/PathWriterProvider.java
+++ readywriter/readywriter-path/src/main/java/org/zzzyxwvut/readywriter/path/internal/PathWriterProvider.java
@@ -7,4 +7,18 @@ import org.zzzyxwvut.readywriter.annotation.Namable;
 @Namable("org.zzzyxwvut.readywriter.PathWriterProvider")
 public interface PathWriterProvider extends ReadyWriter
 {
+	/**
+	 * Creates an instance of this service provider. It is set to write to
+	 * a new templet-named path, appending to the end of a file and using
+	 * the UTF-8 character set and the big-endian byte order.
+	 *
+	 * @return an instance of this service provider
+	 * @see java.util.ServiceLoader.Provider#type()
+	 * @see org.zzzyxwvut.readywriter.service.Lookup#names()
+	 * @see org.zzzyxwvut.readywriter.service.PathWriterVisitor
+	 */
+	static PathWriterProvider provider()
+	{
+		return new PathWriter();
+	}
 }
