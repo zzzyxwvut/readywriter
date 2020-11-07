38eb2ac76a25651144a3acad68a85a0b5f789394
Aliaksei Budavei <oytis@yandex.ru>
2020-11-07 23:42:56 +0300

    Create a demo module declaration

---
+++ readywriter/readywriter-demo/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/** Defines a demonstration of using {@code ReadyWriter}. */
+module org.zzzyxwvut.readywriter.demo
+{
+	requires static org.zzzyxwvut.julics.annotations;
+
+	requires org.zzzyxwvut.julics.core;
+	requires org.zzzyxwvut.julics.naming;
+	requires org.zzzyxwvut.readywriter.service;
+}
